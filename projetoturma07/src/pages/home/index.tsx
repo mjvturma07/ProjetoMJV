7dfbe8b80e6fc8ea4ad9e9183ab243e9a8ee4c19
Hani <haniaboultaif@gmail.com>
2022-11-10 12:48:21 -0300

    Estruturação inicial e configurações

---
+++ projetoturma07/src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./style"
+
+export default function Main(){
+    return(
+        <Container>
+
+            <h1>Hellow world!</h1>
+            
+        </Container>
+    )
+}
+   
